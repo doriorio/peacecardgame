ea90f61ddab0ad349f5ad80c229cd4f1d9f178b1
doriorio <itsdori@gmail.com>
2019-11-04 10:27:02 -0800

    branched to version3

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 /*----- constants -----*/ 
-//Setup - global variables: n = 52, playerIndex
-//card values
+
 var suits = ['s', 'c', 'd', 'h'];
 var ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', '0J', '0Q', '0K', '0A'];
-var fullDeck = []
+var fullDeck = [];
 suits.forEach(function(e){
     ranks.forEach(function(j,i){
      fullDeck.push(e.concat(j, "- ", i));
     })
 })
 
+//eventually, we need a players object so that the toggling between 
+//x and y functions can be done programmatically
 
 //go back and give values in an object later - 
 //for now use the text at the end for values
@@ -17,28 +18,25 @@ suits.forEach(function(e){
 
 /*----- app's state (variables) -----*/ 
 
-//Setup - state variables: turn, score, cardsPerPlayer
-
 xHand = [];
 yHand = [];
 xCardStatus = [];
 yCardStatus = [];
 
 /*----- cached element references -----*/ 
-//allcards = 52
-var fullStack = document.getElementById('full-deck-hold')
-var playerX = document.getElementById('playerX')
-var playerY = document.getElementById('playerY')
-var playerXCard = document.getElementById('playerXcard')
-var playerYCard = document.getElementById('playerYcard')
-var regularPlay = document.getElementById('temp-normal-play')
-var mediationPlay = document.getElementById('temp-mediation')
-var statusMessage = document.getElementById('status-message')
+
+var fullStack = document.getElementById('full-deck-hold');
+var playerX = document.getElementById('playerX');
+var playerY = document.getElementById('playerY');
+var playerXCard = document.getElementById('playerXcard');
+var playerYCard = document.getElementById('playerYcard');
+var regularPlay = document.getElementById('temp-normal-play');
+var mediationPlay = document.getElementById('temp-mediation');
+var statusMessage = document.getElementById('status-message');
 
 
 /*----- event listeners -----*/ 
-//first, click the fulldeck hold box to give both players half the deck
-fullStack.addEventListener('click',shuffleDeck);
+fullStack.addEventListener('click',alert);
 // playerX.addEventListener('click',alert);
 // playerY.addEventListener('click',alert);
 // playerXCard.addEventListener('click',alert);
@@ -47,31 +45,14 @@ regularPlay.addEventListener('click',gamePlay);
 mediationPlay.addEventListener('click',invokeMediation);
 
 //use this as a test function when setting up event listeners or as a placeholder
-function alert(){
-    console.log('alert');
-}
-
-
-//A countdown displays and if there are two clicks (one per player) on the arena, the click listener is activated
-//both players have to pick their square
-//then it looks at the cards on the play div and whoever has the lowest value splices them into their hand
-//if the value is the same. enter mediation
-
-//Looks at the value of both cards and compares (this will be programmatically generated based on  card number)
-
-////If one is higher than the other then both cards are collected
-
-///// If they are the same value, _mediation_ begins
-
-
-
 /*----- functions -----*/
+function alert(){
+    console.log('placeholder function');
+}
 
 function shuffleDeck() {
     var tempDeck = fullDeck.slice();
     var shuffleAll = [];
-
-    // var yHand = [];
     while (tempDeck.length) {
         var rndIdx = (Math.floor(Math.random() * tempDeck.length))
         shuffleAll.push(tempDeck.splice(rndIdx, 1)[0])
@@ -102,59 +83,56 @@ function gamePlay(){
     yPick(1);
     xCardValue = '';
     xCardStatus.forEach(function(e){
-        let xScore = 0
-        xScore = parseInt((e.toString()).split("-",2)[1]);
-        var xCardValue = xScore;
-        return xCardValue;
+        xCardValue = parseInt((e.toString()).split("-",2)[1]);
     })
     yCardValue = '';
     yCardStatus.forEach(function(e){
-        let yScore = 0
-        yScore = parseInt((e.toString()).split("-",2)[1]);
-        var yCardValue = yScore;
-        return yCardValue;
+        yCardValue = parseInt((e.toString()).split("-",2)[1]);
     })
-
-    if (xCardValue < yCardValue){
-        xHand.push(Array.from(yCardStatus));
-        playerX.innerHTML = xHand.toString();
-
-    }
-    if (yCardValue < xCardValue){
-        yHand.push(Array.from(xCardStatus))
-        playerY.innerHTML = yHand.toString();
-    }
-    
-    else if (xCardValue === yCardValue){
-        invokeMediation();
-        console.log('mediation invoked');
-
-    }
     checkForWin();
-}    
+    updateValues();
+}
 
-
-
-function invokeMediation(){
-    alert();
+//ideally the innerHTML setting in x&yPick happens here eventually
+function updateValues(){
+    playerX.innerHTML = xHand;
+    playerY.innerHTML = yHand;
 }
 
 function xPick(num){
     playerXCard.innerHTML = xHand.splice(0,num);
     xCardStatus.push(playerXCard.innerHTML);
-    playerX.innerHTML = xHand;
+
 }    
 function yPick(num){
-    
     playerYCard.innerHTML = yHand.splice(0,num);
     yCardStatus.push(playerYCard.innerHTML);
-    playerY.innerHTML = yHand;
 }    
 
 function checkForWin(){
-    console.log(yHand.length)
-    console.log(xHand.length)
+    if (xCardValue < yCardValue){
+        xHand.push(yCardStatus);
+        console.log(xHand + 'xhand');
+        console.log(yCardStatus + 'ycardstatus');
+    }
+    if (yCardValue < xCardValue){
+        yHand.push(xCardStatus);
+    } 
+    else if (xCardValue === yCardValue){
+        invokeMediation();
+        
+    }
+    
 }
+function invokeMediation(){
+    alert();
+}
+function init(){
+    shuffleDeck();
+    
+}
+
+init();
 
 //render
 //init
